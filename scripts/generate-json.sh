26376705e6dbaad3ec89efc73be429a994966113
Wu, Zhenyu <wuzhenyu@ustc.edu>
2024-12-22 17:56:28 +0800

    Add more icons from eza
    Use scripts/generate-json.sh to generate:
    
    - assets/json/extension.json
    - assets/json/basename.json

---
+++ scripts/generate-json.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -e
+cd "$(dirname "$(dirname "$(readlink -f "$0")")")"
+
+scripts/generate-basename.json.pl eza/src/output/icons.rs | jq -S . > assets/json/extra/basename.json
+scripts/generate-extension.json.pl eza/src/output/icons.rs | jq -S . > assets/json/extra/extension.json
